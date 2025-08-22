49ccbcc3ef56811fb56fe841f2434f26339be940
Peddireddy Pavan Kumar Reddy <99220040683@klu.ac.in>
2025-08-22 11:48:56 +0530

    Update Account.java

--- Online Banking System/Account.java
+++ Online Banking System/Account.java
@@ -1,12 +1,33 @@
-public class Account {
-    private int accountId;
-    private int userId;
+class Account {
+    private String accountNumber;
+    private String holderName;
     private double balance;
+    private List<String> services = new ArrayList<>();
 
-    public Account(int accountId, int userId, double balance) {
-        this.accountId = accountId;
-        this.userId = userId;
+    public Account(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
         this.balance = balance;
     }
-    // deposit, withdraw, check balance methods
+
+    public String getAccountNumber() { return accountNumber; }
+    public String getHolderName() { return holderName; }
+    public double getBalance() { return balance; }
+
+    public void deposit(double amount) { balance += amount; }
+    public boolean withdraw(double amount) {
+        if (amount > balance) return false;
+        balance -= amount;
+        return true;
+    }
+
+    public void addService(String service) { services.add(service); }
+
+    public List<String> getServices() { return services; }
+
+    @Override
+    public String toString() {
+        return accountNumber + " | " + holderName + " | Balance: ₹" + balance +
+               " | Services: " + services;
+    }
 }
